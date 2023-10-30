895c909d5f9e957a5a0ac083b80f5cbcb785485d
MANITEJA KUNTA <129951439+Maniteja1122@users.noreply.github.com>
2023-10-30 17:08:14 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+let type = new Typed("#changing", {
+    strings: ["Web developer", "Python developer", "Freelancer", "Frontend developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
